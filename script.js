5e9f6205c093c8eccb59049767f5daa870a1985b
karla <karlayongzea@gmail.com>
2022-09-19 11:37:40 -0500

    trabajo ahorcado

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
+//Elemtos declaroados en el Html
+//Contenedor de la palabra
 const wordContainer = document.getElementById('wordContainer');
+//Contenedor del boton
 const startButton = document.getElementById('startButton');
+//Contenedor de la palabra que se van usando
 const usedLettersElement = document.getElementById('usedLetters');
 
 let canvas = document.getElementById('canvas');
@@ -7,18 +11,25 @@ let ctx = canvas.getContext('2d');
 ctx.canvas.width  = 0;
 ctx.canvas.height = 0;
 
+
+//Valores para dibujar el hombre del ahorcado con arrays
 const bodyParts = [
-    [4,2,1,1],
-    [4,3,1,2],
+    [4,2,1,1], //cabeza
+    [4,3,1,2], //torzo 
     [3,5,1,1],
     [5,5,1,1],
     [3,3,1,1],
     [5,3,1,1]
 ];
 
+//Variables del juego
+//Palabra adivinar
 let selectedWord;
+//Letras usadas
 let usedLetters;
+//errores
 let mistakes;
+//aciertos
 let hits;
 
 const addLetter = letter => {
@@ -71,6 +82,7 @@ const letterEvent = event => {
     };
 };
 
+//la palabra a utilizar
 const drawWord = () => {
     selectedWord.forEach(letter => {
         const letterElement = document.createElement('span');
@@ -81,23 +93,28 @@ const drawWord = () => {
     });
 };
 
+
+//Seleccionar el array de la palabras
 const selectRandomWord = () => {
     let word = words[Math.floor((Math.random() * words.length))].toUpperCase();
     selectedWord = word.split('');
 };
 
+//Para dibujar al ahorcado
 const drawHangMan = () => {
     ctx.canvas.width  = 120;
     ctx.canvas.height = 160;
     ctx.scale(20, 20);
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.fillStyle = '#d95d39';
+    //Para dibujar la partes de la estructura
     ctx.fillRect(0, 7, 4, 1);
     ctx.fillRect(1, 0, 1, 8);
     ctx.fillRect(2, 0, 3, 1);
     ctx.fillRect(4, 1, 1, 1);
 };
 
+//funcion startGeme al hacer clipLimpiar todas las variables utilzadas
 const startGame = () => {
     usedLetters = [];
     mistakes = 0;
